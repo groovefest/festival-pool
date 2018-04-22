d0b0794458beb2be0c04902e387d1e6d783c1b19
artodev <dev@arto.cash>
2018-04-22 13:57:13 +0700

    Initial Commit
    Arto Pool Source Code

---
+++ website/config.js
@@ -0,0 +1,25 @@
+var api = "http://pool.arto.cash:8117";
+
+var api_blockexplorer = "";
+
+var poolHost = "pool.arto.cash";
+
+var irc = "";
+
+var email = "dev@arto.cash";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "rto": [
+		["pool.arto.cash", "http://pool.arto.cash:8117"]
+    ]
+};
